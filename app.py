664ca177f5dae96e458263f3a2017e83be8ec2ed
Sabbir Bin Abdul Latif <sabbir0727@gmail.com>
2023-09-30 21:44:19 +0600

    Added core files

---
+++ app.py
@@ -0,0 +1,24 @@
+from PyPDF2 import PdfReader
+
+from model import clean_resume, clf, tfidf
+
+reader = PdfReader('resume.pdf')
+
+my_resume = ""
+
+for i in range(len(reader.pages)):
+
+    page = reader.pages[i]
+    page_text = page.extract_text()
+    my_resume += page_text
+
+cleaned_resume = clean_resume(my_resume)
+input_features = tfidf.transform([cleaned_resume])
+prediction_id = clf.predict(input_features)[0]
+
+category_mapping = {0: 'Advocate', 1: 'Arts', 2: 'Automation Testing', 3: 'Blockchain', 4: 'Business Analyst', 5: 'Civil Engineer', 6: 'Data Science', 7: 'Database', 8: 'DevOps Engineer', 9: 'DotNet Developer', 10: 'ETL Developer', 11: 'Electrical Engineering',
+                    12: 'HR', 13: 'Hadoop', 14: 'Health and fitness', 15: 'Java Developer', 16: 'Mechanical Engineer', 17: 'Network Security Engineer', 18: 'Operations Manager', 19: 'PMO', 20: 'Python Developer', 21: 'SAP Developer', 22: 'Sales', 23: 'Testing', 24: 'Web Designing'}
+
+category_name = category_mapping.get(prediction_id, "Unknown")
+
+print("Predicted Category:", category_name)
---
+++ model.py
@@ -0,0 +1,63 @@
+import pickle
+import re
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
+
+df = pd.read_csv('UpdatedResumeDataSet.csv')
+
+df['Category'].value_counts()
+
+df['Category'].unique()
+
+
+def clean_resume(text):
+    clean_text = re.sub('http\S+\s', ' ', text)
+    clean_text = re.sub('RT|cc', ' ', clean_text)
+    clean_text = re.sub('#\S+\s', ' ', clean_text)
+    clean_text = re.sub('@\S+', '  ', clean_text)
+    clean_text = re.sub('[%s]' % re.escape(
+        """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', clean_text)
+    clean_text = re.sub(r'[^\x00-\x7f]', ' ', clean_text)
+    clean_text = re.sub('\s+', ' ', clean_text)
+    return clean_text
+
+
+df['Resume'] = df['Resume'].apply(lambda x: clean_resume(x))
+
+df['Resume']
+
+le = LabelEncoder()
+
+le.fit(df['Category'])
+df['Category'] = le.transform(df['Category'])
+
+df['Category']
+
+df['Resume'][0]
+
+tfidf = TfidfVectorizer(stop_words='english')
+tfidf.fit(df['Resume'])
+required_text = tfidf.transform(df['Resume'])
+
+x_train, x_test, y_train, y_test = train_test_split(
+    required_text, df['Category'], test_size=0.2, random_state=42)
+
+x_train.shape
+
+x_test.shape
+
+clf = OneVsRestClassifier(KNeighborsClassifier())
+clf.fit(x_train, y_train)
+ypred = clf.predict(x_test)
+
+pickle.dump(tfidf, open('tfidf.pkl', 'wb'))
+pickle.dump(clf, open('clf.pkl', 'wb'))
